0561037e57ce95da9ba0987a4a73cccf494ed055
Jonathancollinet <jonathan.collinet@epitech.eu>
2015-09-20 17:25:07 +0200

    interpolate mustaches angular

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,34 @@
 
 var picolApp = angular.module('picolApp', ['ngRoute']);
 
-picolApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+picolApp.config(function($routeProvider, $interpolateProvider) {
 
-	
-
-}]);
-
-picolApp.config(function($routeProvider) {
+	$interpolateProvider.startSymbol('[[');
+	$interpolateProvider.endSymbol(']]');
 
 	$routeProvider
 		.when('/', {
 			templateUrl: '../templates/home.html',
-			controller: 'homeController'
+			controller: 'mainController'
 		});
+
 
 });
 
+picolApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+
+	$log.warn('yo');
+
+	$scope.links = [
+		{item: "#/createCard", label: "Créer une carte"},
+		{item: "#/createDeck", label: "Créer un deck"},
+		{item: "#/addCardToDeck", label: "Ajouter une carte dans un deck"},
+	];
+
+	console.log($scope);
+
+}]);
+
 picolApp.controller('homeController', ['$scope', '$log', function($scope, $log) {
-	$scope.toto = "ototo";
+
 }]);
