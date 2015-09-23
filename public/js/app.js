31c1c084ce6a124b41aba8f28859287a2eb93d2f
LUC TERRACHER <lucterracher@gmail.com>
2015-09-23 21:43:56 +0200

    createCard OK

--- public/js/app.js
+++ public/js/app.js
@@ -44,15 +44,31 @@ picolApp.controller('homeController', ['$scope', '$log', function($scope, $log) 
 
 }]);
 
-picolApp.controller('createCardCtrl', ['$scope', '$log', function($scope, $log) {
+picolApp.controller('createCardCtrl', ['$scope', '$log', '$firebaseArray', function($scope, $log, $firebaseArray) {
 
-	$scope.card = {};
-
+	var ref = new Firebase("https://picolike.firebaseio.com/cards");
+	var ref2 = new Firebase("https://picolike.firebaseio.com/decks");
+	
+	$scope.cards = $firebaseArray(ref);
+	$scope.decks = $firebaseArray(ref2);
+	
 	$scope.submit = function() {
-		console.log("submit !");
-		console.log($scope.card);
+		$scope.cards.$add({dn: $scope.cards.dn,
+		name: $scope.cards.new, desc: $scope.cards.description,
+							color: $scope.cards.color
+											});
 	}
-
+	
+	$scope.cards.$loaded()
+		.then(function(data) {
+			$(document).ready(function(){
+				for (var i = 0; i < data.length; i++) {
+					console.log("data: ", data[i].color);
+					$scope.cards.cardColor = data[i].color.toString();
+					console.log($scope.cards.cardColor);
+				};
+			});
+		});
 }]);
 
 picolApp.controller('createDeckCtrl', ['$scope', '$log', '$firebaseArray', function($scope, $log, $firebaseArray) {
@@ -62,9 +78,8 @@ picolApp.controller('createDeckCtrl', ['$scope', '$log', '$firebaseArray', funct
 	$scope.decks =  $firebaseArray(ref);
 
 	$scope.submit = function() {
-		$scope.decks.$add({name: $scope.decks.new });
+		$scope.decks.$add({name: $scope.decks.new, num: $scope.decks.num });
 	}
-
 
 }]);
 
